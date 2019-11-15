bd0d046c1420d84aa0ffff19ccbb5a404f3e2dfd
Jackie Wayne <jackiewayne2046@163.com>
2019-11-15 12:35:06 +0800

    update

--- app/src/main/java/com/example/jack/mobcomdemo/MainActivity.java
+++ app/src/main/java/com/example/jack/mobcomdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jack.mobcomdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
