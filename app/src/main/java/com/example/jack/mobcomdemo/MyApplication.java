7d69e9f0fd7dc65be6f6d8ed8fae3ff85c2ef19a
shijiewei <jackiewayne2046@163.com>
2020-07-09 14:21:19 +0800

    update

--- app/src/main/java/com/example/jack/mobcomdemo/MyApplication.java
+++ app/src/main/java/com/example/jack/mobcomdemo/MyApplication.java
@@ -1,5 +1,6 @@
 package com.example.jack.mobcomdemo;
 
+import android.os.StrictMode;
 import android.util.Log;
 
 import com.mob.MobApplication;
@@ -11,5 +12,21 @@ public class MyApplication extends MobApplication {
 	public void onCreate() {
 		Log.d(TAG, "Application onCreated");
 		super.onCreate();
+		enableStrictMode();
+	}
+
+	private void enableStrictMode() {
+//		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//				.detectDiskReads()
+//				.detectDiskWrites()
+//				.detectNetwork()   // or .detectAll() for all detectable problems
+//				.penaltyLog()
+//				.build());
+//		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+//				.detectLeakedSqlLiteObjects()
+//				.detectLeakedClosableObjects()
+//				.penaltyLog()
+//				.penaltyDeath()
+//				.build());
 	}
 }
